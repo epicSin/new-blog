97ef540b6fb912798df3398cc756851accaac389
epicSun <eugenmarianov@gmail.com>
2025-11-20 21:47:08 +0200

    фывфв

--- index.php
+++ index.php
@@ -1,104 +1,14 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-</head>
-<body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid justify-content-center">
-        <a class="navbar-brand" href="#">Blog</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">About</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-<div class="container text-center">
-    <h1 class="mb-5">Welcome to my blog!</h1>
-    <div class="row justify-content-center gy-4 mb-4">
-        <div class="col-auto">
-            <div class="card" style="width: 18rem;">
-                <img src="https://dummyimage.com/300x200/7fd2ff/000000" class="card-img-top mx-auto d-block" alt="...">
-                <div class="card-body d-flex flex-column align-items-center">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card’s content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-auto">
-            <div class="card" style="width: 18rem;">
-                <img src="https://dummyimage.com/300x200/7fd2ff/000000" class="card-img-top mx-auto d-block" alt="...">
-                <div class="card-body d-flex flex-column align-items-center">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card’s content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-auto">
-            <div class="card" style="width: 18rem;">
-                <img src="https://dummyimage.com/300x200/7fd2ff/000000" class="card-img-top mx-auto d-block" alt="...">
-                <div class="card-body d-flex flex-column align-items-center">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card’s content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row justify-content-center gy-4">
-        <div class="col-auto">
-            <div class="card" style="width: 18rem;">
-                <img src="https://dummyimage.com/300x200/7fd2ff/000000" class="card-img-top mx-auto d-block" alt="...">
-                <div class="card-body d-flex flex-column align-items-center">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card’s content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-auto">
-            <div class="card" style="width: 18rem;">
-                <img src="https://dummyimage.com/300x200/7fd2ff/000000" class="card-img-top mx-auto d-block" alt="...">
-                <div class="card-body d-flex flex-column align-items-center">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card’s content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-auto">
-            <div class="card" style="width: 18rem;">
-                <img src="https://dummyimage.com/300x200/7fd2ff/000000" class="card-img-top mx-auto d-block" alt="...">
-                <div class="card-body d-flex flex-column align-items-center">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card’s content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-    </div>
+<?php
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-            crossorigin="anonymous"></script>
-</body>
-</html>
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require './vendor/autoload.php';
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
